84df96e9e4e3ad7b7aef55a3a72431dcf1b3cc6b
bharaths1803 <bharaths14051803@gmail.com>
2025-07-24 23:10:06 +0530

    Changed user route

--- devtinder-backend/src/route/chat.route.js
+++ devtinder-backend/src/route/chat.route.js
@@ -7,6 +7,6 @@ console.log("✅ Loading chat.route.js");
 
 const router = Router();
 
-router.get("/:toUserId", userAuth, fetchMessages);
+router.get("/dm/:toUserId", userAuth, fetchMessages);
 
 module.exports = router;
